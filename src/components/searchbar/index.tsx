35993f04630fba5ba6c4b9cfc4ab982557786e14
choi <joeplay0801@gmail.com>
2025-04-29 17:15:42 +0900

    feat: 사진 검색기능 구현

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -2,19 +2,38 @@ import styles from "./SearchBar.module.scss";
 import searchIcon from "@/assets/search.svg";
 import imageIcon from "@/assets/image.svg";
 import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
 
 function SearchBar() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState<File | null>(null);
   const navigate = useNavigate();
+  console.log(image);
   const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       const query = (event.target as HTMLInputElement).value;
       navigate(`/search?term=${query}`);
     }
   };
+  const handleImageClick = () => {
+    inputRef.current?.click();
+  };
   return (
     <div className={styles.searchBar}>
-      <img src={imageIcon} alt="search" className={styles.imageIcon} />
+      <img
+        src={imageIcon}
+        alt="search"
+        className={styles.imageIcon}
+        onClick={handleImageClick}
+      />
       <img src={searchIcon} alt="search" className={styles.searchIcon} />
+      <input
+        type="file"
+        accept="image/*"
+        hidden
+        ref={inputRef}
+        onChange={(e) => setImage(e.target.files?.[0] || null)}
+      />
       <input
         type="text"
         placeholder="여행지 설명을 작성해주세요"
@@ -23,7 +42,15 @@ function SearchBar() {
           handleSearch(e);
         }}
       />
-      <input type="image" hidden />
+      {image && (
+        <div className={styles.imagePreview}>
+          <img
+            src={URL.createObjectURL(image)}
+            alt="preview"
+            className={styles.previewImage}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,7 +1,7 @@
-import Header from "../../components/header";
+import Layout from "../../components/layout";
 
 function home() {
-  return <Header />;
+  return <Layout>aa</Layout>;
 }
 
 export default home;
